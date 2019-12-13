fad6eb64928c8ce7f5d86544b8b5208e7fa55ed8
autumnFish <517729329@qq.com>
2019-12-13 12:21:09 +0800

    登录模块 - 整体布局

--- src/main.js
+++ src/main.js
@@ -10,6 +10,9 @@ import 'element-ui/lib/theme-chalk/index.css';
 // 注册一下
 Vue.use(ElementUI);
 
+// 导入全局样式
+import "./style/base.css";
+
 
 Vue.config.productionTip = false
 
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,13 +1,36 @@
 <template>
-  <div> 我是登录页面</div>
+  <div class="login-box">
+    <div class="form-box"></div>
+    <img class="login-pic" src="../../assets/login_banner_ele.png" alt="" />
+  </div>
 </template>
 
 <script>
 export default {
-    name:"login"
-}
+  name: "login"
+};
 </script>
 
-<style>
-
+<style lang="less">
+.login-box {
+  /* 开启弹性布局 */
+  display: flex;
+  /* 上下居中 */
+  align-items: center;
+  /* 左右 均分 */
+  justify-content: space-around;
+  background: linear-gradient(
+    225deg,
+    rgba(20, 147, 250, 1),
+    rgba(1, 198, 250, 1)
+  );
+  height: 100%;
+  .form-box {
+    width: 478px;
+    height: 550px;
+    background-color: #f5f5f5;
+  }
+  .login-pic {
+  }
+}
 </style>

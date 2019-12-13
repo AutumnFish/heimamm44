c893062b8a1f47f0a0dfb9e1dbbcbda0ced26653
autumnFish <517729329@qq.com>
2019-12-13 11:20:47 +0800

    项目起步 - Element-ui整合

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
+  <div>
+  <el-button type="success">成功按钮</el-button>
   <router-view></router-view>
+
+  </div>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 // 导入接收 路由
 import router from './router/router.js'
+// 导入饿了么 ui
+import ElementUI from 'element-ui';
+// 导入饿了么 的样式
+import 'element-ui/lib/theme-chalk/index.css';
+// 注册一下
+Vue.use(ElementUI);
 
 
 Vue.config.productionTip = false

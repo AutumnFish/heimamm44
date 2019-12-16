38d9826c3e3d4af356b2c02525b48fe292131e14
autumnFish <517729329@qq.com>
2019-12-16 12:26:44 +0800

    首页模块 - 顶部布局

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,7 +1,18 @@
 <template>
 <!-- 布局容器 -->
   <el-container class="index-container">
-    <el-header class="my-header">Header</el-header>
+    <el-header class="my-header">
+      <div class="left">
+        <i  class="el-icon-s-fold icon"></i>
+        <img class="logo" src="../../assets/index_logo.png" alt="">
+        <span class="title">黑马面面</span>
+      </div>
+      <div class="right">
+        <img class="user-icon" src="../../assets/classflower.jpg" alt="">
+        <span class="user-name">李达,您好</span>
+        <el-button type="primary" size="small">退出</el-button>
+      </div>
+    </el-header>
     <el-container>
       <el-aside width="200px" class="my-aside">Aside</el-aside>
       <el-main class="my-main">Main</el-main>
@@ -20,6 +31,39 @@ export default {
   height: 100%;
   .my-header{
     background-color: skyblue;
+    display: flex;
+    justify-content: space-between;
+    .left{
+      display: flex;
+      align-items: center;
+      .icon{
+        font-size: 24px;
+        margin-right: 22px;
+      }
+      .logo{
+        width: 33px;
+        height: 28px;
+        margin-right: 11px;
+      }
+      .title{
+        font-size: 22px;
+        color:#49A1FF;
+      }
+    }
+    .right{
+      display: flex;
+      align-items: center;
+      .user-icon{
+        border-radius: 50%;
+        width: 43px;
+        height: 43px;
+        margin-right: 9px;
+      }
+      .user-name{
+        font-size: 14px;
+        margin-right: 38px;
+      }
+    }
   }
   .my-aside{
     background-color: green;

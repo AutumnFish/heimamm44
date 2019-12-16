39dd695644347a1554aa488de7ff98b860765a08
autumnFish <517729329@qq.com>
2019-12-16 12:12:41 +0800

    首页模块 - 整体布局

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,13 +1,31 @@
 <template>
-  <div> 我是首页！！！</div>
+<!-- 布局容器 -->
+  <el-container class="index-container">
+    <el-header class="my-header">Header</el-header>
+    <el-container>
+      <el-aside width="200px" class="my-aside">Aside</el-aside>
+      <el-main class="my-main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-    name:"index"
-}
+  name: "index"
+};
 </script>
 
-<style>
-
+<style lang="less">
+.index-container{
+  height: 100%;
+  .my-header{
+    background-color: skyblue;
+  }
+  .my-aside{
+    background-color: green;
+  }
+  .my-main{
+    background-color: purple;
+  }
+}
 </style>

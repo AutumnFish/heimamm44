39943f6f53776f96ca54f952c34fa331b87b38ab
autumnFish <517729329@qq.com>
2019-12-16 14:48:46 +0800

    首页模块 - 左侧导航整合

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,20 +1,44 @@
 <template>
-<!-- 布局容器 -->
+  <!-- 布局容器 -->
   <el-container class="index-container">
     <el-header class="my-header">
       <div class="left">
-        <i  class="el-icon-s-fold icon"></i>
-        <img class="logo" src="../../assets/index_logo.png" alt="">
+        <i class="el-icon-s-fold icon"></i>
+        <img class="logo" src="../../assets/index_logo.png" alt="" />
         <span class="title">黑马面面</span>
       </div>
       <div class="right">
-        <img class="user-icon" src="../../assets/classflower.jpg" alt="">
+        <img class="user-icon" src="../../assets/classflower.jpg" alt="" />
         <span class="user-name">李达,您好</span>
         <el-button type="primary" size="small">退出</el-button>
       </div>
     </el-header>
     <el-container>
-      <el-aside width="200px" class="my-aside">Aside</el-aside>
+      <!-- 导航菜单 -->
+      <el-aside width="200px" class="my-aside">
+        <el-menu default-active="2" class="el-menu-vertical-demo">
+          <el-menu-item index="1">
+            <i class="el-icon-pie-chart"></i>
+            <span slot="title">数据概览</span>
+          </el-menu-item>
+          <el-menu-item index="2">
+            <i class="el-icon-user"></i>
+            <span slot="title">用户列表</span>
+          </el-menu-item>
+          <el-menu-item index="3">
+            <i class="el-icon-edit-outline"></i>
+            <span slot="title">题库列表</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <i class="el-icon-office-building"></i>
+            <span slot="title">企业列表</span>
+          </el-menu-item>
+          <el-menu-item index="5">
+            <i class="el-icon-notebook-2"></i>
+            <span slot="title">学科列表</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
       <el-main class="my-main">Main</el-main>
     </el-container>
   </el-container>
@@ -27,48 +51,48 @@ export default {
 </script>
 
 <style lang="less">
-.index-container{
+.index-container {
   height: 100%;
-  .my-header{
-    background-color: skyblue;
+  .my-header {
+    // background-color: skyblue;
     display: flex;
     justify-content: space-between;
-    .left{
+    .left {
       display: flex;
       align-items: center;
-      .icon{
+      .icon {
         font-size: 24px;
         margin-right: 22px;
       }
-      .logo{
+      .logo {
         width: 33px;
         height: 28px;
         margin-right: 11px;
       }
-      .title{
+      .title {
         font-size: 22px;
-        color:#49A1FF;
+        color: #49a1ff;
       }
     }
-    .right{
+    .right {
       display: flex;
       align-items: center;
-      .user-icon{
+      .user-icon {
         border-radius: 50%;
         width: 43px;
         height: 43px;
         margin-right: 9px;
       }
-      .user-name{
+      .user-name {
         font-size: 14px;
         margin-right: 38px;
       }
     }
   }
-  .my-aside{
-    background-color: green;
+  .my-aside {
+    // background-color: green;
   }
-  .my-main{
+  .my-main {
     background-color: purple;
   }
 }

5b8a1f19749e1d75ec4a681b449df02e64fddf38
autumnFish <517729329@qq.com>
2019-12-19 15:57:49 +0800

    学科列表 - 删除学科

--- src/views/index/subject/subject.vue
+++ src/views/index/subject/subject.vue
@@ -4,13 +4,13 @@
     <el-card class="head-card">
       <el-form :inline="true" :model="formInline" class="demo-form-inline">
         <el-form-item label="学科编号">
-          <el-input class="short-input" v-model="formInline.rid" ></el-input>
+          <el-input class="short-input" v-model="formInline.rid"></el-input>
         </el-form-item>
         <el-form-item label="学科名称">
-          <el-input v-model="formInline.name" ></el-input>
+          <el-input v-model="formInline.name"></el-input>
         </el-form-item>
         <el-form-item label="创建者">
-          <el-input class="short-input" v-model="formInline.username" ></el-input>
+          <el-input class="short-input" v-model="formInline.username"></el-input>
         </el-form-item>
         <el-form-item label="状态">
           <el-select v-model="formInline.status" placeholder="请选择状态">
@@ -28,23 +28,23 @@
     <!-- 身体卡片 -->
     <el-card class="body-card">
       <el-table :data="tableData" style="width: 100%">
-        <el-table-column type="index" label="序号" > </el-table-column>
-        <el-table-column prop="rid" label="学科编号" > </el-table-column>
-        <el-table-column prop="name" label="学科名称" > </el-table-column>
+        <el-table-column type="index" label="序号"> </el-table-column>
+        <el-table-column prop="rid" label="学科编号"> </el-table-column>
+        <el-table-column prop="name" label="学科名称"> </el-table-column>
         <el-table-column prop="short_name" label="简称"> </el-table-column>
         <el-table-column prop="username" label="创建者"> </el-table-column>
         <el-table-column prop="create_time" label="创建日期"> </el-table-column>
         <el-table-column prop="status" label="状态">
           <template slot-scope="scope">
-              <span v-if="scope.row.status===1">启用</span>
-              <span class="red" v-else>禁用</span>
+            <span v-if="scope.row.status === 1">启用</span>
+            <span class="red" v-else>禁用</span>
           </template>
         </el-table-column>
-        <el-table-column  label="操作">
+        <el-table-column label="操作">
           <template slot-scope="scope">
             <el-button type="text">编辑</el-button>
-            <el-button type="text">{{ scope.row.status===1?"禁用":"启用"}}</el-button>
-            <el-button type="text">删除</el-button>
+            <el-button type="text">{{ scope.row.status === 1 ? "禁用" : "启用" }}</el-button>
+            <el-button type="text" @click="removeItem(scope.row)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -70,7 +70,7 @@
 // 导入组件
 import addDialog from "./components/addDialog.vue";
 // 导入接口
-import { subjectList } from "../../../api/subject.js";
+import { subjectList, subjectRemove } from "../../../api/subject.js";
 export default {
   name: "subject",
   // 注册组件
@@ -82,13 +82,13 @@ export default {
       // 顶部的 行内表单
       formInline: {
         // 学科id
-        rid:"",
+        rid: "",
         // 状态
-        status:"",
+        status: "",
         // 学科名称
-        name:"",
+        name: "",
         // 创建者
-        username:""
+        username: ""
       },
       // table的数据
       tableData: [
@@ -122,39 +122,62 @@ export default {
       // 每一页多少条
       limit: 2,
       // 页容量选项 数组
-      pageSizes:[2,4,6,9],
+      pageSizes: [2, 4, 6, 9],
       // 总条数
-      total:0
+      total: 0
     };
   },
   created() {
     this.getData();
   },
   methods: {
+    // 删除数据
+    removeItem(item) {
+      window.console.log(item);
+      this.$confirm(`你真的要删除 ${item.intro}`, "友情提示", {
+        confirmButtonText: "确认",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          // 调用移除接口
+          subjectRemove({
+            id: item.id
+          }).then(res=>{
+            // window.console.log(res)
+            if(res.code===200){
+              this.$message.success('删除成功！！');
+              // 调用方法 重新获取数据
+              this.getData()
+            }
+          });
+        })
+        .catch(() => {});
+    },
     // 页码改变
-    handleCurrentChange(page){
+    handleCurrentChange(page) {
       window.console.log(page);
       // 保存页码
-      this.page = page
+      this.page = page;
       // 重新获取数据
-      this.getData()
+      this.getData();
     },
     // 页容量改变 回调函数
-    handleSizeChange(size){
+    handleSizeChange(size) {
       window.console.log(size);
       // 保存 页容量
       this.limit = size;
       // 重新获取数据
-      this.getData()
+      this.getData();
     },
     // 清空搜索
-    clear(){
+    clear() {
       for (const key in this.formInline) {
         // 获取每一个属性
-        this.formInline[key] = ''
-      } 
+        this.formInline[key] = "";
+      }
       // 重新获取数据
-      this.getData()
+      this.getData();
     },
     // 数据获取
     getData() {
@@ -200,8 +223,8 @@ export default {
   }
 
   // 高亮的span
-  span.red{
-    color:#ff4b4b;
+  span.red {
+    color: #ff4b4b;
   }
 }
 </style>
